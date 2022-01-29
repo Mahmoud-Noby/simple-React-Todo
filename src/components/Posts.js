c852c6797f5f44fd46dbd3625c3a4ab911e1bfd8
Mahmoud-Noby <mahmoudnoby146@outlook.com>
2022-01-29 17:13:44 +0200

    Using Axios

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,37 @@
-
 import  { Component } from 'react';
+import axios from 'axios';
 class Posts extends Component {
-
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response)  => {
+      console.log(response.data);
+      this.setState({
+        posts: response.data.slice(0, 11)
+      })
+    })
+  }
   render() {
+    const { posts } = this.state;
+    const postsList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+         )
+      })
+    ) : (
+      <div className="center"> No posts yet </div>
+    )
     return (
       <div className="container">
-        posts
+        <h3 className="center blue-text">Posts</h3>
+        {postsList}
       </div>
     )
   }
